e27678f3f6a66f4f2f97d23cc2b82a1e36743301
bes <besufikadtilahun1994@gmail.com>
2024-04-22 14:53:12 +0300

    timer component added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import Header from "./Header.vue"; 
+import Header from "./Header.vue";
+import Timer from "./Timer.vue";
 </script>
 
 <template>
   <Header />
+  <div class="container">
+    <Timer />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
